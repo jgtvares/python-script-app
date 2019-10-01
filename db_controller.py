378d45d330ae421cf6ec8d59d4b84bc1d262da42
Joao Tavares <joaogabrielsilvatavares@gmail.com>
2019-10-01 01:52:21 -0300

    update: new files

---
+++ db_controller.py
@@ -0,0 +1,49 @@
+import sqlite3
+from model import Script
+
+
+class DBController:
+    db_name = 'script.db'
+    connection = None
+    table = """CREATE TABLE SCRIPT(
+                            STORY TEXT,
+                            SCENE TEXT,
+                            CHARACTER TEXT,
+                            LINE TEXT,
+                            TEXT_COLOR TEXT)"""
+
+    insert = "INSERT INTO SCRIPT(STORY, SCENE, CHARACTER, LINE, TEXT_COLOR) VALUES(?,?,?,?,?)"
+
+    select = "SELECT STORY, SCENE, CHARACTER, LINE, TEXT_COLOR FROM SCRIPT"
+
+    select_where = "SELECT STORY, SCENE, CHARACTER, LINE, TEXT_COLOR FROM SCRIPT WHERE STORY = ?"
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.connection = sqlite3.connect(self.db_name)
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(self.table)
+            self.connection.commit()
+        except:
+            print('Database already created! Creation ignored.')
+
+        cursor.close()
+
+    def save_script(self, script):
+        register = (script.story, script.scene, script.line, script.character, script.text_color)
+        cursor = self.connection.cursor()
+        cursor.execute(self.insert, register)
+        self.connection.commit()
+        cursor.close()
+
+    def return_script_as_list(self):
+        cursor = self.connection.cursor()
+        cursor.execute(self.select)
+        script_list = cursor.fetchall()
+        self.connection.commit()
+        cursor.close()
+
+        return script_list
+
