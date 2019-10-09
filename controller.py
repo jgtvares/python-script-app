9d39f3856e1563df057e373929f507c7477edcd5
João Tavares <joaogabrielsilvatavares@gmail.com>
2019-10-09 20:39:35 -0300

    funcionando

--- controller.py
+++ controller.py
@@ -1,3 +1,5 @@
+from macpath import norm_error
+
 from view import App
 from model import Script
 from db_controller import DBController
@@ -7,7 +9,7 @@ import webbrowser
 
 
 class Controller(object):
-    scriptView = App(create_cmd=None, load_cmd=None, play_cmd=None)
+    scriptView = None # App(create_cmd=None, load_cmd=None, play_cmd=None)
     db_ctrl = None
     saved = None
     textList = []
@@ -24,26 +26,14 @@ class Controller(object):
         self.scriptView.load_scripts(scripts)
         self.all_scripts_list.append(scripts)
 
-        print(scripts)
-
     def play(self):
         self.saved = self.scriptView.read()
         story = self.saved.__str__()
         self.db_ctrl.read_script(story)
         self.scriptView.play(self.saved)
 
-        'self.db_ctrl.read_script()'
-        """_list = self.db_ctrl.all_scripts()
-
-        for line in _list:
-            script = Script(line[0], line[1], line[2], line[3], line[4])
-            self.scriptView.read()
-            self.textList.append(script)
-
-            print(self.textList)"""
-
     def exec(self):
         self.db_ctrl = DBController()
-        self.scriptView = App(self.insert, self.load(), self.play())
+        self.scriptView = App(self.insert, self.load, self.play)
 
         self.scriptView.exec()
